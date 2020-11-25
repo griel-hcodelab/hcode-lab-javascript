a598c37556cf140af0d5616cac986cdab8a03b48
Roberto Griel Filho <roberto@griel.com.br>
2020-11-25 19:36:10 -0300

    Executando JavaScript com o Node

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Olá Mundo");
+
+let a = 1;
+let b = 2;
+
+console.log(a + b);
+
+function calcular(a,b,o){
+    return eval(`${a} ${o} ${b}`);
+}
+
+console.log(calcular(13, 49, '*'));
