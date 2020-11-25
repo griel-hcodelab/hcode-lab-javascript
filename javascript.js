62b298357c3a93de24d8a19b83fd1ae0b7c7147b
Roberto Griel Filho <roberto@griel.com.br>
2020-11-25 19:12:47 -0300

    Trabalhando com arquivos externos

--- javascript.js
+++ javascript.js
@@ -5,3 +5,16 @@ console.log("Mensagem");
 console.error("Mensagem de Erro");
 
 let tagH2 = document.querySelector('h2');
+let botao = document.querySelector("#trocaCor");
+
+//console.log(getComputedStyle(tagH2, null).getPropertyValue('font-size')); //Pegar valores CSS
+
+//
+
+//console.log(tagH2.style.color);
+
+let funcaoEvento = function() {
+    tagH2.style.color = 'yellow';
+}
+
+botao.addEventListener("click", funcaoEvento);
