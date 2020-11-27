cbe0c54445245dad61c9fe823602b5abaac180b5
Roberto Griel Filho <roberto@griel.com.br>
2020-11-27 15:19:01 -0300

    Aula de var e Hoisting

---
+++ hoisting.js
@@ -0,0 +1,10 @@
+//Hoisting existe só no JavaScript.
+
+ano = 1989;
+
+console.log(ano);
+
+//O hoisting iça a variavel em relação à declaração dela, não em relação à inicialização
+
+var nome = "Roberto";
+console.log(nome);
