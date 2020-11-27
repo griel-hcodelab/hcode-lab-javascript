35942f7abda1507618a2c32b51bab0f22729de93
Roberto Griel Filho <roberto@griel.com.br>
2020-11-27 16:55:05 -0300

    Aula de Precedência de Operador

---
+++ precedencia-operador.js
@@ -0,0 +1,11 @@
+//A multiplicação e divisão vem antes da soma e subtração
+console.log(10 + 5 * 4);
+//Vou obrigar a soma antes da multiplicação
+console.log((10 + 5) * 4);
+//String com número irá concatenar
+console.log("10" + 10);
+console.log("10" + (10 + 10));
+
+let numeroInicial = 5;
+console.log(++numeroInicial);
+console.log(++numeroInicial + 10);
