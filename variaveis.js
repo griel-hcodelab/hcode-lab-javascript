1b48577e81c9376da1ea836b2fc73b447a93512f
Roberto Griel Filho <roberto@griel.com.br>
2020-11-27 15:09:13 -0300

    Aula de Nomear Variáveis

---
+++ variaveis.js
@@ -0,0 +1,13 @@
+//O comando mais usado para criar variável
+var nome = "Roberto";
+console.log(nome);
+
+//Variável não pode ser criada com números e caracteres especiais
+
+//É possível criar várias variáveis na mesma linha, mas não é uma boa prática
+var anoAtual = 2020,
+  anoPassado = 1995,
+  diferencaAnos = anoAtual - anoPassado;
+console.log(diferencaAnos);
+
+//É uma boa prática declarar variáveis em inglês
