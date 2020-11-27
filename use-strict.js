c7b39475d90daf947af1fd17b3e1836bbef3bf4f
Roberto Griel Filho <roberto@griel.com.br>
2020-11-27 15:52:15 -0300

    Aula de modo estrito

---
+++ use-strict.js
@@ -0,0 +1,8 @@
+"use strict";
+/* Irá desativar todos os recursos legados, como por exemplo o hoisting e deve ser sempre a primeira linha
+ *  Dessa forma o código se tornará mais seguro
+ */
+
+let empresa = "Griel";
+
+console.log(empresa);
