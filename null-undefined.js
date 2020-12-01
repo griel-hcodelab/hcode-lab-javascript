114dd7509e880c54414987df11f0f541248a2b68
Roberto Griel Filho <roberto@griel.com.br>
2020-12-01 11:49:18 -0300

    Aula de Null e Undefined

---
+++ null-undefined.js
@@ -0,0 +1,8 @@
+//Undefined = Algo que não foi definido, por exemplo varável declarada mas não definida
+//Null = Algo que não existe
+
+let pessoa;
+console.log(pessoa); //A variável existe mas não contém valor
+
+let nome = null;
+console.log(nome); //A variavel existe mas o conteúdo dentro dela não existe
