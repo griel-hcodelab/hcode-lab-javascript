fc2e5c01b1a6fb570683d72ebe774c2ad843a84b
Roberto Griel Filho <roberto@griel.com.br>
2020-12-01 18:36:18 -0300

    Aula de IF

---
+++ if.js
@@ -0,0 +1,11 @@
+const valor = 1000;
+
+if (valor === 100) {
+  console.log("É igual a 100");
+} else if ((valor >= 100 && valor === 1000) || valor === 5000) {
+  console.log("É maior que 100");
+} else if (valor < 200) {
+  console.log("É menor que 200");
+} else {
+  console.log("É menor que 100");
+}
