38790b18ce35e540e2beac7d69eff67926e06c35
Roberto Griel Filho <roberto@griel.com.br>
2020-12-02 18:38:43 -0300

    Aula de Arrays com shift e unshift

---
+++ shift-unshift.js
@@ -0,0 +1,5 @@
+const paises = ["Brasil", "Argentina", "Uruguai"];
+paises.unshift("Chile"); //Adiciona um item no começo do array
+//console.log(paises);
+
+console.log(paises.shift()); //Remove um item do começo do array
