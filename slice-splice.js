787234e1f1c9c7e8d4491b5ecf30f8c184ff7e6f
Roberto Griel Filho <roberto@griel.com.br>
2020-12-02 18:57:12 -0300

    Aula de Array com slice e splice

---
+++ slice-splice.js
@@ -0,0 +1,8 @@
+const paises = ["Brasil", "Argentina", "Uruguai", "Estados Unidos", "Itália"];
+
+//console.log(paises.slice(2, 4));
+//paises.splice(2, 2);
+paises.splice(2, 3, "Paraguai", "Venezuela", "Iraque");
+//Indico onde eu quero começar a busca, depois qual indice eu quero remover, e posso adicionar outros no lugar
+
+console.log(paises);
