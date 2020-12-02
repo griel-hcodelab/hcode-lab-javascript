faca8b8b68ac51d00158e2709e51a2772fd84f08
Roberto Griel Filho <roberto@griel.com.br>
2020-12-02 19:31:58 -0300

    Aula de Array com spread operator

---
+++ spread-operator.js
@@ -0,0 +1,6 @@
+const carrosDoJoao = ["Porshe", "Lamborghini", "Gurgel"];
+const carrosdoRafael = ["Ferrari", "Palio 98", "Fusca"];
+
+const carros = [...carrosDoJoao, ...carrosdoRafael];
+console.log(carros);
+//(6) ['Porshe', 'Lamborghini', 'Gurgel', 'Ferrari', 'Palio 98', 'Fusca']
