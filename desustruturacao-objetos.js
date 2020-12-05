ac3867a9a1da4bb694ad975483391795d37ac1d4
Roberto Griel Filho <roberto@griel.com.br>
2020-12-05 15:33:49 -0300

    Aula de desestruturação com objetos

---
+++ desustruturacao-objetos.js
@@ -0,0 +1,10 @@
+const pessoa = {
+  nome: "Roberto",
+  idade: 31,
+  email: "roberto@griel.com.br",
+  senha: 123456,
+};
+
+const { nome, idade, senha } = pessoa;
+console.log();
+console.log(nome, idade, senha); //Roberto 31 123456
