d608ba139a5f970130f8dc0254b9cb3ff4a234a1
Roberto Griel Filho <roberto@griel.com.br>
2020-12-13 11:08:28 -0300

    Anotações

---
+++ projeto-express/anotacoes.js
@@ -0,0 +1,11 @@
+/**
+ * Criar um package.json de forma simplificada: npm init -y
+ * Instalar o servidor Express: npm install express
+ * Rodar o servidor: node ARQUIVO.JS (Como produção)
+ *
+ * Nodemon: Reinicia o servidor sempre que há alguma alteração
+ * npm install nodemon --save-dev
+ *
+ * Instando o NeDB, banco de dados simples e leve
+ * npm install nedb
+ */
